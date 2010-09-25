34d68a0e690e4dbe4deef867ff5d919844ae4c1c
Bosko Ivanisevic <bosko.ivanisevic@gmail.com>
2010-09-25 17:21:17 +0200

    Process foreign and primary keys

--- lib/generator/generator.rb
+++ lib/generator/generator.rb
@@ -16,7 +16,7 @@ module ModelGenerator
     end
 
     def connect
-      return if db_settings.empty? || db_settings.nil?
+      return if @connection_options.empty?
       
       ActiveRecord::Base.establish_connection(@connection_options)
       @connection = ActiveRecord::Base.connection
@@ -32,23 +32,69 @@ module ModelGenerator
     
     def create_model(table_name)
       File.open(File.join(output_path, "#{table_name}.rb"), "w") do |file|
-        file.write generate_model_source(table_name)
+        constraints = []
+        foreign_keys.each do |fk|
+          src = constraint_src(table_name, fk)
+          constraints << src unless src.nil?
+        end
+        
+        file.write generate_model_source(table_name, constraints)
       end
     end
 
     def process?(table_name, include_prefixes)
+      return true if include_prefixes.empty?
+      
       include_prefixes.each do |prefix|
         return true if table_name =~ /^#{prefix}/
       end
+
       false
     end
+
+    def foreign_keys
+      @foreign_keys ||= fetch_foreign_keys
+    end
+
+    private
+    def fetch_foreign_keys
+      fk = []
+      case @connection_options[:adapter]
+      when 'mysql'
+        fk = connection.select_all(mysql_fk_sql)
+      end
+    end
+
+    def constraint_src(table_name, fk={})
+      src = nil
+      if fk['from_table'] == table_name
+        src = "belongs_to :#{fk['to_table']}, :class_name => '#{fk['to_table'].classify}', :foreign_key => :#{fk['from_column']}"
+      elsif fk['to_table'] == table_name
+        src = "has_many :#{fk['from_table'].pluralize}, :class_name => '#{fk['from_table'].classify}'"
+      end
+      src
+    end
     
-    def generate_model_source(table_name)
-      src = <<-EOS
-class #{table_name.camelize} < ActiveRecord::Base
-  set_table_name '#{table_name}'
-end
-EOS
+    def generate_model_source(table_name, constraints)
+      src = "class #{table_name.classify} < ActiveRecord::Base\n"
+      primary_key = connection.primary_key(table_name)
+      src << "  set_primary_key :#{primary_key}\n" unless "id" == primary_key || primary_key.nil?
+      src << "  set_table_name '#{table_name}'\n" unless table_name == table_name.classify.tableize
+      src << "  #{constraints.join("\n  ")}"
+      src << "\nend\n"
+    end
+
+    def mysql_fk_sql
+      sql = <<-SQL
+select
+ table_name as from_table,
+ column_name as from_column,
+ referenced_table_name as to_table,
+ referenced_column_name as to_column
+from information_schema.KEY_COLUMN_USAGE
+where referenced_table_schema like '%'
+ and referenced_table_name is not null
+SQL
     end
   end
 end
